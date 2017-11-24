e974959f22858340deaa1a9c3003a4e73072d9c7
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2017-11-24 15:48:46 -0500

    Clean up sessions after request (even with Except)
    When a query is issued and the flask function returns without committing the
    session, the DB connection gets blocked forever, exhausting the pool of
    connections.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
 from werkzeug.serving import run_simple
 from myads_service import app
+from myads_service.models import db
 
 application = app.create_app()
+
+@application.teardown_request
+def shutdown_session(exception=None):
+     db.session.remove()
 
 if __name__ == "__main__":
     run_simple('0.0.0.0', 5000, application, use_reloader=False, use_debugger=False)
