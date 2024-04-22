5f586c320ac5ed583ba84f9c8b6e8b7b2cf94367
femalves <fernandamalves@ufrn.edu.br>
2024-04-22 14:27:53 -0400

    adding revision

---
+++ alembic/versions/15025c9be4f6_changed_sort.py
@@ -0,0 +1,28 @@
+"""changed sort
+
+Revision ID: 15025c9be4f6
+Revises: ffdbd392dc89
+Create Date: 2024-04-22 14:27:13.360604
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '15025c9be4f6'
+down_revision = 'ffdbd392dc89'
+
+from alembic import op
+import sqlalchemy as sa
+
+                               
+
+
+def upgrade():
+    #with app.app_context() as c:
+    #   db.session.add(Model())
+    #   db.session.commit()
+        
+    pass
+
+
+def downgrade():
+    pass
